6cd4e9e8e43cbbf7554d139c3d5823a97bf3930d
Pierre-Hugo Meynet <pierreh.meynet@outlook.fr>
2024-08-30 12:22:44 +0200

    add server island + vercel adapter + enhance blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  experimental: {
+    serverIslands: true,
+  },
+  output: "server",
+  adapter: vercel()
 });
